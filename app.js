67e23d95140b2bf148f8b250a03dbf0ac3a2ed3f
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-08-12 02:26:37 +0300

    const_addEventListener

--- app.js
+++ app.js
@@ -1,48 +1,17 @@
-let circles = document.querySelector(".circles");
-let submit = document.querySelector("#x");
-let span = document.getElementsByTagName("span");
-let sec1 = document.querySelector(".sec1");
-// let sec2 = document.classList.contains(“sec2”);
-sec1.addEventListener("click",(e) => {
-  if(e.target.classList.contains("one")){
-    e.target.style.backgroundColor=`rgb(127,136,150)`;
-    e.target.style.color="white";
-    sec1.style.visibility = "hidden";
-    sec2.style.visibility = "visible";
-    console.log("2 clicked");
-    var x = 1;
-  }
-  else if(e.target.classList.contains("two")){
-    e.target.style.backgroundColor=`rgb(127,136,150)`;
-    e.target.style.color="white";
-    console.log("2 clicked");
-    var x = 2;
-    }
-    else if(e.target.classList.contains("three")){
-        e.target.style.backgroundColor=`rgb(127,136,150)`;
-        e.target.style.color="white";
-        console.log("3 clicked");
-        var x = 2;
-    }
-    else if(e.target.classList.contains("four")){
-        e.target.style.backgroundColor=`rgb(127,136,150)`;
-        e.target.style.color="white";
-        console.log("4 clicked");
-        var x = 2;
-    }
-    else if(e.target.classList.contains("five")){
-        e.target.style.backgroundColor=`rgb(127,136,150)`;
-        e.target.style.color="white";
-        console.log("5 clicked");
-        var x = 2;
-    }
-   else{
-    console.log("other element clicked");
-   }
-})
-// const
-// submit.addEventListener(“click”,()=>{
-//   sec1.style.visibility = “hidden”;
-//   sec2.style.visibility = “visible”
-//   span.innerText = e.target.innerText
-//   })
+const buttons = document.querySelector(".main-button");
+const submit = document.querySelector(".submit");
+const sec1 = document.querySelector("#section-1");
+const sec2 = document.querySelector("#section-2");
+const span = document.querySelector(".number4");
+let number;
+buttons.addEventListener("click" ,(e) => {
+number = e.target.innerHTML
+// e.target.style.backgraounColor = “grey”;
+//    console.log(number);
+console.log(number);
+submit.addEventListener("click",() => {
+    sec2.style.display = " block" ;
+    sec1.style.display = "none";
+    span.innerHTML = number ;
+});
+});
