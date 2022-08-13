cd0757ce5c4137f9e691cf08410ea419f120af64
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-08-13 03:06:28 +0300

    select_button_fix

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const sec1 = document.querySelector("#section-1");
 const sec2 = document.querySelector("#section-2");
 const span = document.querySelector(".number4");
 let number;
-buttons.addEventListener("click" ,(e) => {
-number = e.target.innerHTML
-// e.target.style.backgraounColor = “grey”;
+buttons.addEventListener("click",(e) => {
+number = e.target;
+number.style.backgroundColor = "black";
 //    console.log(number);
-console.log(number);
-submit.addEventListener("click",() => {
+
+submit.addEventListener ("click",() => {
     sec2.style.display = " block" ;
     sec1.style.display = "none";
-    span.innerHTML = number ;
-    console.log( "You selected:" ,`${e}`)
+    span.innerHTML = number.innerHTML ;
+    // console.log( "You selected:" ,`${e}`)
 });
 });
